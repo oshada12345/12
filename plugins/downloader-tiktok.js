15f8c2a22ea5723072a358ee399facbcaa112300
oshadaXD <135181748+oshada12345@users.noreply.github.com>
2023-06-16 13:16:11 +0530

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,16 +1,16 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
-if (!args[0]) throw `Linknya Mana?`
+if (!args[0]) throw `Where's the link?🌝`
   let res = await fetch(`https://api.zeltoria.my.id/api/download/tiktok?url=${args[0]}&apikey=${global.zeltoria}`)
   let x = await res.json()
   let anu = x.result
-  let cap = `*Nih Kak  Videonya* `
+  let cap = `*🍃Here is the video🍃* `
   conn.sendFile(m.chat, anu.video.no_watermark, 'tiktok.mp4', cap, m)
 }
 handler.help = ['tiktok']
 handler.tags = ['downloader']
 handler.command = /^(tiktok|tt|ttdl|tiktokdl)$/i
-handler.limit = true
+handler.limit = 
 
 export default handler
